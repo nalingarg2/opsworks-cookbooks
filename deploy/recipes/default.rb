aa98ef5c557d44c2c96ad6f64efcfa97ada58ba2
nalin <ng1181@nyu.edu>
2014-09-01 09:51:19 -0400

    Update default.rb
    python-pip

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -5,45 +5,13 @@ node[:deploy].each do |application, deploy|
   opsworks_deploy_user do
     deploy_data deploy
   end
-
+  
 execute "install_update" do
 	command "apt-get update"
 end
-  
-  package "git" do
+
+package "python-pip" do 
 	action :install
-end
-
-
-directory "/git" do
-	mode 0755
-	owner "ubuntu" 
-	group "ubuntu" 
-	action :create
-end
-
-package "ngnix" do
-	action :install
-end
-
-service "ngnix" do
-	action[:enable,:start]
-	suport :reload => true
-	notifies :reload
-end
-
-package "gunicorn" do
-	action :install
-end
-
-service "gunicorn" do
-	action[:enable,:start]
-	suport :reload => true
-	notifies :reload
-end
-
-python_pip "django" do
-  version "1.1.4"
 end
 
 
